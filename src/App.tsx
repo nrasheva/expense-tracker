2cbbc021fb0e61b8dd614b99a2579c045fac7b75
nrasheva <nadezhda.rasheva96@gmail.com>
2023-03-09 21:17:56 +0200

    Implement income date selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { add, isBefore, startOfDay } from 'date-fns';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedDate, setSelectedDate] = useState(1);
+  const [checked, setChecked] = useState(false);
+  const [nextIncome, setNextIncome] = useState('');
+
+  const handleConfirmation = (): void => {
+    // Get midnight UTC date of user selection
+    const today = startOfDay(new Date());
+    const incomeDay = startOfDay(new Date().setDate(Number(selectedDate)));
+
+    let nextIncome = incomeDay;
+
+    if (checked) {
+      nextIncome = add(incomeDay, {
+        months: 1,
+      });
+    } else if (!checked && isBefore(incomeDay, today)) {
+      setNextIncome('Income not received by selected date');
+      return;
+    }
+
+    setNextIncome(nextIncome.toString());
+  };
 
   return (
-    <div className="App">
+    <div className='app'>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>What day do you get paid?</p>
+        <input
+          max='31'
+          min='1'
+          onChange={(e) => setSelectedDate(Number(e.target.value))}
+          type='number'
+          value={selectedDate}
+        />
+        <button onClick={handleConfirmation}>Confirm</button>
+        <input checked={checked} id='received' onChange={(e) => setChecked(e.target.checked)} type='checkbox'></input>
+        <label htmlFor='received'>Received for current period</label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {nextIncome}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
